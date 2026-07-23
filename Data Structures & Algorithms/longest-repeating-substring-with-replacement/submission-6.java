73c5ce11484e4cff2a5706d0eef500942ce3bffb
sharmadigember35-oss <sharmadigember35@gmail.com>
2026-07-23 11:55:09 +0530

    Add: longest-repeating-substring-with-replacement - submission-6

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-6.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        // borut force code to solve this problem think----
+        /*int res =0;
+        for(int i=0;i<s.length();i++){
+            HashMap<Character,Integer> hash = new HashMap<>();
+             int max =Integer.MIN_VALUE;
+            for(int j=i;j<s.length();j++){
+                hash.put(s.charAt(j),hash.getOrDefault(s.charAt(j),0)+1);
+              max = Math.max(max,hash.get(s.charAt(j)));
+
+               if((j-i+1)-max<=k){
+                res = Math.max(res,j-i+1);
+               }
+            }
+        }
+        return res;*/
+        // now solve by sliding window 
+        // instead of loop under O(n^2)  we can use slidning window and two pint 
+        int left =0;
+        int res =0;
+        int max = Integer.MIN_VALUE;
+        HashMap<Character,Integer> hash = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            hash.put(s.charAt(i),hash.getOrDefault(s.charAt(i),0)+1);
+            max = Math.max(max,hash.get(s.charAt(i)));
+            if((i-left+1)-max >k){
+                hash.put(s.charAt(left),hash.get(s.charAt(left))-1);
+                left++;
+            }
+              if((i-left+1)-max<=k){
+                res = Math.max(res,i-left+1);
+              }
+             
+            
+             
+        }
+
+        return res;
+    }
+}
