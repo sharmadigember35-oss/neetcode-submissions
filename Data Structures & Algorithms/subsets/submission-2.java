302e6b1844f8caf7667f96568786d06495c25230
sharmadigember35-oss <sharmadigember35@gmail.com>
2026-07-24 07:50:19 +0530

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+     static ArrayList<List<Integer>> l = new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        l.clear();
+        ArrayList<Integer>list = new ArrayList<>();
+          find_answer(nums,0,list);
+         return l;
+        
+    }
+    public void find_answer(int[] nums , int index , List<Integer> list){
+         
+            if(index == nums.length){
+                  l.add(new ArrayList<>(list));
+                  return  ;
+            }
+            list.add(nums[index]);
+            find_answer(nums,index+1,list);
+            list.remove(list.size()-1);
+            find_answer(nums,index+1,list);
+    }
+}
